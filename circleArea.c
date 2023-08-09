aacfbce672621687c163583767b703b93e02c504
TamonudSharma <73441372+spidey711@users.noreply.github.com>
2023-08-09 19:49:51 +0700

    Code to find area of circle

---
+++ circleArea.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    int r, area;
+    printf("Enter radius of circle: ");
+    scanf("%d", &r);
+    area = 3.14 * r * r;
+    printf("\nArea of circle is: %d sq units.", area);
+
+}

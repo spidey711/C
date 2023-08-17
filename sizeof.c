a89931aef98617a366cb021aafdaca5f09140a8f
TamonudSharma <73441372+spidey711@users.noreply.github.com>
2023-08-17 21:40:52 +0700

    program to find size of each data type

---
+++ sizeof.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+    char a;
+    float b;
+    int c;
+    short int d;
+    long int e;
+    long long int f;
+    double g;
+    long double h;
+
+
+    printf("%d", sizeof(a));
+    printf("\n%d", sizeof(b));
+    printf("\n%d", sizeof(c));
+    printf("\n%d", sizeof(d));
+    printf("\n%d", sizeof(e));
+    printf("\n%d", sizeof(f));
+    printf("\n%d", sizeof(g));
+    printf("\n%d", sizeof(h));
+
+}

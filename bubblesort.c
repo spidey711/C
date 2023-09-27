bd2fb227ab0722ddf05137313055f0d7dd49645d
TamonudSharma <73441372+spidey711@users.noreply.github.com>
2023-09-27 14:23:20 +0700

    made it a function that can be called

--- bubblesort.c
+++ bubblesort.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
-int main(){
+int bubbleSort(arrSize){
+    
+    int i, j, temp;
 
-    int i, j, temp, n;
-
-    printf("Enter number of elements:\n");
-    scanf("%d", &n);
-
-    int arr[n];
+    int arr[arrSize];
 
     printf("Enter number of elements:\n");
-    for (i=0; i<n; i++){
+    for (i=0; i<arrSize; i++){
         scanf("%d", &arr[i]);
     }
 
-    for (i=0; i<n-1; i++){
-        for (j=0; j<n-i-1; j++){
+    for (i=0; i<arrSize-1; i++){
+        for (j=0; j<arrSize-i-1; j++){
             if (arr[j] > arr[j+1]){
                 temp = arr[j];
                 arr[j] = arr[j+1];
@@ -25,8 +22,29 @@ int main(){
     }
 
     printf("\nBubble Sorted Array\n");
-    for (i=0; i<n; i++){
+    for (i=0; i<arrSize; i++){
         printf("%d\n", arr[i]);
+    }
+
+}
+
+int main(){
+
+    int arrSize, run = 1;
+
+    while (run == 1){ 
+
+        printf("Enter number of elements:\n");
+        scanf("%d", &arrSize);
+        
+        bubbleSort(arrSize);
+        
+        run = 0;
+        printf("\nDo you wish to continue? y(1)/n(0): ");
+        scanf("%d", &run);
+        if (run == 0){
+            break;
+        }
     }
 
     return 0;
